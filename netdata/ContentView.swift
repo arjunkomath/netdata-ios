64e2036a04c3886a0b433f3c6f9824adbee02ddc
Arjun Komath <arjunkomath@gmail.com>
2020-07-11 12:41:56 +1000

    Initial Commit

---
+++ netdata/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  netdata
+//
+//  Created by Arjun Komath on 11/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ netdata/netdataApp.swift
@@ -0,0 +1,17 @@
+//
+//  netdataApp.swift
+//  netdata
+//
+//  Created by Arjun Komath on 11/7/20.
+//
+
+import SwiftUI
+
+@main
+struct netdataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
